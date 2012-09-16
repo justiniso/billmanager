86d4f962d6a5f0828080200f7c8a81ff4416c17a
Justin Iso <justin.james.iso@gmail.com>
2012-09-15 23:59:57 -0400

    .gitignore and db install

--- billmanager/settings.py
+++ billmanager/settings.py
@@ -1,3 +1,5 @@
+import os
+
 # Django settings for billmanager project.
 
 DEBUG = True
@@ -10,14 +12,10 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(os.path.dirname(__file__), 'pinshout.db')
+   }
 }
 
 # Local time zone for this installation. Choices can be found here:
