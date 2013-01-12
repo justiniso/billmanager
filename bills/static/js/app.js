9661bff127d545e7fc2755735a255e579c1df045
Justin Iso <justin.james.iso@gmail.com>
2013-01-11 18:01:05 -0800

    added confirmation bindings for forms and actions

--- bills/static/js/app.js
+++ bills/static/js/app.js
@@ -2,10 +2,29 @@
 
 function setup() {
 
+	// TABLES
+
 	// make table rows clickable if they have a data-href attribute
-	$('tr[data-href]').click(function() {
+	$('tr[data-href]').click( function() {
 		var href = $(this).data('href');
 		window.location = href;
+	});
+
+
+	// FORMS & ACTIONS
+	$('.confirm').click( function() {
+		var conf = confirm('Are you sure?')
+		return conf;
+	});
+
+	$('.confirm-delete').click( function() {
+		var conf = confirm('Are you sure you want to delete this?')
+		return conf;
+	});
+
+	$('.confirm-remove-friend').click( function() {
+		var conf = confirm('Are you sure you want to remove this friend?')
+		return conf;
 	});
 
 }
