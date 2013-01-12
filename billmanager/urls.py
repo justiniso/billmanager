1b02e991d7025b2d57403282cf67ecf3ddd44473
Justin Iso <justin.james.iso@gmail.com>
2013-01-11 16:58:33 -0800

    url pattern for adding a friend

--- billmanager/urls.py
+++ billmanager/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     # url for all bills /bills/
     url(r'^bills/(\d+)/$', 'bills.views.show_bill', name='bill'),
     url(r'^bills/(\d+)/delete/$', 'bills.views.delete_bill', name='delete_bill'),
+    url(r'^users/(\d+)/add/$', 'bills.views.add_user', name='add_user'),
     url(r'^login/$', 'bills.views.login', name='login'),
     url(r'^register/$', 'bills.views.register', name='register'),
     url(r'^logout/$', 'bills.views.logout', name='logout'),
