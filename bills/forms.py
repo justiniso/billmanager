20dd920ecd29ae0f53921a50bd29a0532b174699
Justin Iso <justin.james.iso@gmail.com>
2013-01-16 23:31:57 -0800

    added explicit recipient field to bill form and changed all username field types to email fields

--- bills/forms.py
+++ bills/forms.py
@@ -1,14 +1,18 @@
-# TODO: move to a new forms file
-# from django.app import forms
+
 from django import forms
 from django.forms import ModelForm
 from bills.models import *
+from bills.form_fields import *
 
 class CreateBillForm(ModelForm):
+	recipient = forms.EmailField(max_length=100)
 	class Meta:
 		model = Bill
 		fields = ('item', 'amount', 'due_date', 'message')
-		exlude = ('creator', )
+		exclude = ('creator', )
+
+	def clean(self, *args, **kwargs):
+		return super(CreateBillForm, self).clean(*args, **kwargs)
 
 class DeleteBillForm(forms.ModelForm):
 	class Meta:
@@ -16,11 +20,11 @@ class DeleteBillForm(forms.ModelForm):
 		fields = []
 
 class LoginForm(forms.Form):
-	username = forms.CharField(max_length=100)
+	username = forms.EmailField(max_length=100)
 	password = forms.CharField(widget=forms.PasswordInput(render_value=False),max_length=100)
 
 class RegisterForm(forms.Form):
-	username = forms.CharField(max_length=100)
+	username = forms.EmailField(max_length=100)
 	password = forms.CharField(widget=forms.PasswordInput(render_value=False),max_length=100)
 	first_name = forms.CharField(max_length=24)
 	last_name = forms.CharField(max_length=24)
