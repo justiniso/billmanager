a9a25b463db49852b2acce352a942a52c77feb86
Justin Iso <justin.james.iso@gmail.com>
2013-01-18 00:03:53 -0800

    added email settings

--- billmanager/settings.py
+++ billmanager/settings.py
@@ -167,3 +167,12 @@ LOGGING = {
 
 # for extending user profile
 AUTH_PROFILE_MODULE = 'bills.UserProfile'
+
+
+# email
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'noreplybillmanager@gmail.com'
+EMAIL_HOST_PASSWORD = 'billmanagerpassword'
+
